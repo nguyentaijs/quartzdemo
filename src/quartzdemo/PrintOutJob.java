6442437400db385f2a3f977b86f8292a6fe53e3c
tai <nguyentaijs@gmail.com>
2018-09-14 09:29:47 +0700

    first Job ever!!!

---
+++ src/quartzdemo/PrintOutJob.java
@@ -0,0 +1,15 @@
+package quartzdemo;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class PrintOutJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("First job ever!!!");
+	}
+
+	
+}
--- src/quartzdemo/mainClass.java
+++ src/quartzdemo/mainClass.java
@@ -1,17 +1,34 @@
 package quartzdemo;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
 public class mainClass {
 
 	public static void main(String[] args) {
 		try {
-			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+			// Define Job detail
+			JobDetail firstJobDetail = JobBuilder.newJob(PrintOutJob.class)
+										.withIdentity("first job", "group 1")
+										.build();
+			// Define trigger
+			Trigger firstTrigger = TriggerBuilder.newTrigger()
+									.withIdentity("first trigger", "group 12")
+									.withSchedule(SimpleScheduleBuilder.simpleSchedule()
+																		.withIntervalInSeconds(3)
+																		.repeatForever())
+									.build();
 			
+			// schedule
+			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
 			scheduler.start();
-			
-			scheduler.shutdown();
+			scheduler.scheduleJob(firstJobDetail, firstTrigger);
+			//scheduler.shutdown();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
